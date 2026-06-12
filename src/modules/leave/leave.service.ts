57679102d71943cffdaca9addb1cff001047178f
Trần Đình Nam <trandinhnamuet@gmail.com>
2026-06-13 02:48:28 +0700

    Always soft-delete leave types; getLeaveTypes filters isActive:true only

--- src/modules/leave/leave.service.ts
+++ src/modules/leave/leave.service.ts
@@ -83,12 +83,7 @@ export class LeaveService {
   async deleteLeaveType(id: number) {
     const lt = await this.typeRepo.findOne({ where: { id } });
     if (!lt) throw new NotFoundException('Không tìm thấy loại nghỉ');
-    const inUse = await this.requestRepo.count({ where: { leaveTypeId: id } });
-    if (inUse > 0) {
-      await this.typeRepo.update(id, { isActive: false });
-    } else {
-      await this.typeRepo.remove(lt);
-    }
+    await this.typeRepo.update(id, { isActive: false });
     return { message: 'Đã xóa loại nghỉ' };
   }
 
